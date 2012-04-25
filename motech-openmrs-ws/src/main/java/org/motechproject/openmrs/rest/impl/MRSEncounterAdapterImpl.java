f260ecb255d133f970bf482a9d03fe9704cdf45f
Dave Roberge <david.roberge@maine.edu>
2012-04-25 14:09:19 -0400

    Added encounter adapter implementation

---
+++ motech-openmrs-ws/src/main/java/org/motechproject/openmrs/rest/impl/MRSEncounterAdapterImpl.java
@@ -0,0 +1,220 @@
+package org.motechproject.openmrs.rest.impl;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.JsonNodeFactory;
+import org.codehaus.jackson.node.ObjectNode;
+import org.motechproject.mrs.model.MRSEncounter;
+import org.motechproject.mrs.model.MRSFacility;
+import org.motechproject.mrs.model.MRSObservation;
+import org.motechproject.mrs.model.MRSPatient;
+import org.motechproject.mrs.model.MRSPerson;
+import org.motechproject.mrs.services.MRSEncounterAdapter;
+import org.motechproject.mrs.services.MRSException;
+import org.motechproject.mrs.services.MRSPatientAdapter;
+import org.motechproject.openmrs.rest.DateUtil;
+import org.motechproject.openmrs.rest.HttpException;
+import org.motechproject.openmrs.rest.JsonConverterUtil;
+import org.motechproject.openmrs.rest.RestfulClient;
+import org.motechproject.openmrs.rest.url.OpenMrsEncounterUrlHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MRSEncounterAdapterImpl implements MRSEncounterAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(MRSEncounterAdapterImpl.class);
+
+	private final MRSPatientAdapter patientAdapter;
+	private final RestfulClient restfulClient;
+	private final OpenMrsEncounterUrlHolder encounterUrl;
+
+	@Autowired
+	public MRSEncounterAdapterImpl(RestfulClient restfulClient, MRSPatientAdapter patientAdapter,
+			OpenMrsEncounterUrlHolder encounterUrl) {
+		this.restfulClient = restfulClient;
+		this.patientAdapter = patientAdapter;
+		this.encounterUrl = encounterUrl;
+	}
+
+	@Override
+	public MRSEncounter createEncounter(MRSEncounter encounter) {
+		ObjectNode encounterObj = JsonNodeFactory.instance.objectNode();
+		encounterObj.put("location", encounter.getFacility().getId());
+		encounterObj.put("encounterType", encounter.getEncounterType());
+		encounterObj.put("encounterDatetime", DateUtil.formatToOpenMrsDate(encounter.getDate()));
+		encounterObj.put("patient", encounter.getPatient().getId());
+		encounterObj.put("provider", encounter.getProvider().getId());
+		
+		// web services do not currently allow creator property
+		//encounterObj.put("creator", encounter.getCreator().getId());
+		
+		encounterObj.put("obs", observationsToJson(encounter.getObservations()));
+		try {
+			JsonNode response = restfulClient.postForJsonNode(encounterUrl.getEncounterPath(), encounterObj);
+			return new MRSEncounter(response.get("uuid").asText(), encounter.getProvider(), encounter.getCreator(),
+					encounter.getFacility(), encounter.getDate(), encounter.getPatient(), encounter.getObservations(),
+					encounter.getEncounterType());
+		} catch (HttpException e) {
+			logger.error("Could not create encounter: " + e.getMessage());
+			throw new MRSException(e);
+		}
+	}
+
+	private ArrayNode observationsToJson(Set<MRSObservation> observations) {
+		ArrayNode obsArray = JsonNodeFactory.instance.arrayNode();
+		for (MRSObservation obs : observations) {
+			ObjectNode obsObj = JsonNodeFactory.instance.objectNode();
+
+			obsObj.put("concept", resolveConceptUuidFromConceptName(obs.getConceptName()));
+			obsObj.put("value", obs.getValue().toString());
+			obsObj.put("obsDatetime", DateUtil.formatToOpenMrsDate(obs.getDate()));
+			if (CollectionUtils.isNotEmpty(obs.getDependantObservations())) {
+				obsObj.put("groupMembers", observationsToJson(obs.getDependantObservations()));
+			}
+
+			obsArray.add(obsObj);
+		}
+
+		return obsArray;
+	}
+
+	private String resolveConceptUuidFromConceptName(String conceptName) {
+		try {
+			String encodedConceptName = URLEncoder.encode(conceptName, "UTF-8");
+			JsonNode response = restfulClient.getEntityByJsonNode(encounterUrl
+					.getConceptSearchByName(encodedConceptName));
+			JsonNode results = response.get("results");
+			if (results.size() == 0) {
+				logger.error("No concept found with name: " + conceptName + " (" + encodedConceptName + ")");
+				throw new MRSException(new RuntimeException(
+						"Can't create an encounter because no concept was found with name: " + conceptName));
+			} else if (results.size() > 1) {
+				logger.warn("Found more than 1 concept with name: " + conceptName + " (" + encodedConceptName + ")");
+			}
+
+			return results.get(0).get("uuid").asText();
+		} catch (UnsupportedEncodingException e) {
+			logger.error("Could not URL Encode the concept name: " + conceptName);
+			throw new MRSException(e);
+		} catch (HttpException e) {
+			logger.error("There was an error retrieving the uuid of the concept with concept name: " + conceptName);
+			throw new MRSException(e);
+		}
+	}
+
+	@Override
+	public List<MRSEncounter> getAllEncountersByPatientMotechId(String motechId) {
+		List<MRSEncounter> encounters = new ArrayList<MRSEncounter>();
+		MRSPatient patient = patientAdapter.getPatientByMotechId(motechId);
+
+		if (patient != null) {
+			encounters.addAll(getEncountersForPatient(patient));
+		}
+
+		return encounters;
+	}
+
+	private List<MRSEncounter> getEncountersForPatient(MRSPatient patient) {
+		List<MRSEncounter> encounters = new ArrayList<MRSEncounter>();
+		try {
+			JsonNode resultObj = restfulClient.getEntityByJsonNode(encounterUrl.getEncountersByPatientUuid(patient
+					.getId()));
+			JsonNode resultArray = resultObj.get("results");
+			for (int i = 0; i < resultArray.size(); i++) {
+				JsonNode encounterObj = resultArray.get(i);
+				String encounterType = encounterObj.get("encounterType").get("name").asText();
+				Date encounterDate = DateUtil.parseOpenMrsDate(encounterObj.get("encounterDatetime").asText());
+
+				MRSFacility facility = JsonConverterUtil.convertJsonToMrsFacility(encounterObj.get("location"));
+
+				JsonNode providerObj = getPerson(encounterObj.get("provider").get("uuid").asText());
+				MRSPerson provider = JsonConverterUtil.convertJsonToMrsPerson(providerObj);
+
+				//JsonNode creatorObj = getCreator(encounterObj.get("auditInfo").get("creator").get("uuid").asText());
+				//JsonNode creatorPersonObj = getPerson(creatorObj.get("person").get("uuid").asText());
+				//MRSPerson creatorPerson = JsonConverterUtil.convertJsonToMrsPerson(creatorPersonObj);
+//				MRSUser user = new MRSUser().id(creatorObj.get("uuid").asText()).person(creatorPerson)
+//						.systemId(creatorObj.get("systemId").asText()).userName(creatorObj.get("username").asText());
+
+				MRSEncounter encounter = new MRSEncounter(encounterObj.get("uuid").asText(), provider, null, facility,
+						encounterDate, patient, getObservations(encounterObj.get("obs")), encounterType);
+				encounters.add(encounter);
+			}
+		} catch (HttpException e) {
+			logger.error("Error retrieving encounters for patient: " + patient.getMotechId());
+			throw new MRSException(e);
+		} catch (ParseException e) {
+			logger.error("Error parsing encounter date for patient: " + patient.getMotechId());
+			throw new MRSException(e);
+		}
+
+		return encounters;
+	}
+
+	private JsonNode getPerson(String providerUuid) {
+		try {
+			return restfulClient.getEntityByJsonNode(encounterUrl.getProviderByUuid(providerUuid));
+		} catch (HttpException e) {
+			throw new MRSException(e);
+		}
+	}
+
+	private JsonNode getCreator(String asText) {
+		try {
+			return restfulClient.getEntityByJsonNode(encounterUrl.getCreatorByUuid(asText));
+		} catch (HttpException e) {
+			throw new MRSException(e);
+		}
+	}
+
+	private Set<MRSObservation> getObservations(JsonNode observationArray) throws ParseException {
+		Set<MRSObservation> observations = new HashSet<MRSObservation>();
+		for (int i = 0; i < observationArray.size(); i++) {
+			JsonNode obsObj = observationArray.get(i);
+			observations.add(new MRSObservation(obsObj.get("uuid").asText(), DateUtil.parseOpenMrsDate(obsObj.get(
+					"obsDatetime").asText()), obsObj.get("concept").get("display").asText(), obsObj.get("value")
+					.asText()));
+		}
+
+		return observations;
+	}
+
+	@Override
+	public MRSEncounter getLatestEncounterByPatientMotechId(String motechId, String encounterType) {
+		List<MRSEncounter> previousEncounters = getAllEncountersByPatientMotechId(motechId);
+		Iterator<MRSEncounter> encounterItr = previousEncounters.iterator();
+		
+		// filter out encounters with non matching encounterType
+		while(StringUtils.isNotBlank(encounterType) && encounterItr.hasNext()) {
+			MRSEncounter enc = encounterItr.next();
+			if (!encounterType.equals(enc.getEncounterType())) {
+				encounterItr.remove();
+			}
+		}
+		
+		MRSEncounter latestEncounter = null;
+		for(MRSEncounter enc : previousEncounters) {
+			if (latestEncounter == null) {
+				latestEncounter = enc;
+			} else {
+				latestEncounter = enc.getDate().after(latestEncounter.getDate()) ? enc : latestEncounter;
+			}
+		}
+		
+		return latestEncounter;
+	}
+}
---
+++ motech-openmrs-ws/src/test/java/org/motechproject/openmrs/rest/impl/MRSEncounterAdapterImplIT.java
@@ -0,0 +1,226 @@
+package org.motechproject.openmrs.rest.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.JsonNodeFactory;
+import org.codehaus.jackson.node.ObjectNode;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.motechproject.mrs.model.MRSEncounter;
+import org.motechproject.mrs.model.MRSFacility;
+import org.motechproject.mrs.model.MRSObservation;
+import org.motechproject.mrs.model.MRSPatient;
+import org.motechproject.mrs.model.MRSPerson;
+import org.motechproject.mrs.model.MRSUser;
+import org.motechproject.mrs.services.MRSEncounterAdapter;
+import org.motechproject.openmrs.rest.HttpException;
+import org.motechproject.openmrs.rest.RestfulClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationOpenMrsWS.xml" })
+public class MRSEncounterAdapterImplIT extends AbstractAdapterImplIT {
+
+	private static final String TEST_CONCEPT_NAME = "Test Concept";
+	private static final String CONCEPT_PATH = "/ws/rest/v1/concept";
+	private static final String ENCOUNTER_TYPE = "ADULTINITIAL";
+
+	private MRSFacility facility;
+	private MRSPatient patient;
+	private String tempConceptUuid;
+	private MRSUser creator;	
+	
+	@Autowired
+	MRSEncounterAdapter encounterAdapter;
+
+	@Autowired
+	RestfulClient restfulClient;
+
+	@Value("${openmrs.url}")
+	String openmrsUrl;
+
+	@Before
+	public void before() {
+		facility = null;
+		patient = null;
+		tempConceptUuid = null;
+		creator = null;		
+	}
+	
+	@Test
+	public void shouldCreateEncounter() throws HttpException, URISyntaxException {
+		MRSEncounter persistedEncounter = null;
+		try {
+			createRequiredEntities();
+			
+			persistedEncounter = createEncounterWithSingleObservation();
+			assertNotNull(persistedEncounter.getId());
+		} finally {
+			deleteEncounter(persistedEncounter);
+			deleteCreatedEntities();
+		}
+	}
+
+	private MRSEncounter createEncounterWithSingleObservation() {
+		MRSEncounter persistedEncounter;
+		Set<MRSObservation> obs = new HashSet<MRSObservation>();
+		MRSObservation ob = new MRSObservation(Calendar.getInstance().getTime(), TEST_CONCEPT_NAME, "Test Value");
+		obs.add(ob);
+
+		persistedEncounter = createEncounter(obs, currentDate);
+		return persistedEncounter;
+	}
+	
+	@Test
+	public void shouldFindEncounter() throws HttpException, URISyntaxException {
+		MRSEncounter persistedEncounter = null;
+		try {
+			createRequiredEntities();
+			persistedEncounter = createEncounterWithSingleObservation();
+			
+			List<MRSEncounter> encounters = encounterAdapter.getAllEncountersByPatientMotechId(MOTECH_ID_1);
+			assertEquals(1, encounters.size());
+			
+			MRSEncounter fetchedEncounter = encounters.get(0);
+			assertEquals(persistedEncounter.getId(), fetchedEncounter.getId());
+			// this property currently cannot be set through the web services
+			assertNull(fetchedEncounter.getCreator());
+			assertNotNull(fetchedEncounter.getProvider());
+			assertNotNull(fetchedEncounter.getFacility());
+			assertNotNull(fetchedEncounter.getPatient());
+		} finally {
+			deleteEncounter(persistedEncounter);
+			deleteCreatedEntities();
+		}
+	}
+	
+	@Test 
+	public void shouldFindLatestEncounter() throws HttpException, URISyntaxException {
+		List<MRSEncounter> persistedEncounters = null;
+		try {
+			createRequiredEntities();
+			persistedEncounters = createMultipleEncounterWithObservation();
+			
+			MRSEncounter encounter = encounterAdapter.getLatestEncounterByPatientMotechId(MOTECH_ID_1, null);
+			assertEquals(currentDate, encounter.getDate());
+		} finally {
+			deleteEncounter(persistedEncounters.get(0));
+			deleteEncounter(persistedEncounters.get(1));
+			deleteCreatedEntities();
+		}		
+	}
+
+	private List<MRSEncounter> createMultipleEncounterWithObservation() {
+		List<MRSEncounter> encounters = new ArrayList<MRSEncounter>();
+		MRSEncounter persistedEncounter;
+		Set<MRSObservation> obs = new HashSet<MRSObservation>();
+		MRSObservation ob = new MRSObservation(Calendar.getInstance().getTime(), TEST_CONCEPT_NAME, "Test Value");
+		obs.add(ob);
+
+		persistedEncounter = createEncounter(obs, currentDate);
+		encounters.add(persistedEncounter);
+		
+		Calendar pastDate = Calendar.getInstance();
+		pastDate.add(Calendar.DATE, -100);
+		obs = new HashSet<MRSObservation>();
+		ob = new MRSObservation(Calendar.getInstance().getTime(), TEST_CONCEPT_NAME, "Test Value");
+		obs.add(ob);
+		
+		persistedEncounter = createEncounter(obs, pastDate.getTime());
+		encounters.add(persistedEncounter);
+		
+		return encounters;
+    }
+
+	private MRSEncounter createEncounter(Set<MRSObservation> obs, Date encounterDate) {
+		MRSEncounter encounter = new MRSEncounter(creator.getPerson().getId(), creator.getId(), facility.getId(),
+				encounterDate, patient.getId(), obs, ENCOUNTER_TYPE);
+
+		return encounterAdapter.createEncounter(encounter);		
+	}
+
+	private void createRequiredEntities() throws HttpException, URISyntaxException {
+		facility = createTemporaryLocation();
+		patient = createTemporaryPatient(MOTECH_ID_1, makePerson(), facility);
+		tempConceptUuid = createTemporaryConcept();
+		creator = createTemporaryProvider();
+	}	
+	
+	private void deleteCreatedEntities() throws HttpException, URISyntaxException {
+		deleteConcept(tempConceptUuid);
+		deletePatient(patient);
+		deleteUser(creator);
+		deleteFacility(facility);
+	}
+
+	private String createTemporaryConcept() throws URISyntaxException, HttpException {
+		URI uri = new URI(openmrsUrl + CONCEPT_PATH);
+		ObjectNode conceptObj = JsonNodeFactory.instance.objectNode();
+		conceptObj.put("name", TEST_CONCEPT_NAME);
+		conceptObj.put("datatype", "Text");
+		conceptObj.put("conceptClass", "Test");
+		JsonNode result = restfulClient.postForJsonNode(uri, conceptObj);
+
+		return result.get("uuid").asText();
+	}
+
+	private MRSUser createTemporaryProvider() throws URISyntaxException, HttpException {
+		ObjectNode person = JsonNodeFactory.instance.objectNode();
+		person.put("birthdate", "1970-01-01");
+		person.put("gender", "M");
+		ArrayNode node = JsonNodeFactory.instance.arrayNode();
+		ObjectNode preferredName = JsonNodeFactory.instance.objectNode();
+		preferredName.put("givenName", "Troy");
+		preferredName.put("familyName", "Parks");
+		node.add(preferredName);
+		person.put("names", node);
+
+		URI personUri = new URI(openmrsUrl + "/ws/rest/v1/person");
+		JsonNode response = restfulClient.postForJsonNode(personUri, person);
+		String personUuid = response.get("uuid").asText();
+
+		ObjectNode userNode = JsonNodeFactory.instance.objectNode();
+		userNode.put("username", "troy");
+		userNode.put("password", "Testing123");
+		userNode.put("person", personUuid);
+
+		URI userUri = new URI(openmrsUrl + "/ws/rest/v1/user");
+		response = restfulClient.postForJsonNode(userUri, userNode);
+
+		return new MRSUser().id(response.get("uuid").asText()).person(new MRSPerson().id(personUuid));
+	}
+
+	private void deleteEncounter(MRSEncounter persistedEncounter) throws HttpException, URISyntaxException {
+		if (persistedEncounter == null) return;
+		restfulClient.deleteEntity(new URI(openmrsUrl + "/ws/rest/v1/encounter/" + persistedEncounter.getId() + "?purge"));
+	}	
+	
+	private void deleteConcept(String tempConceptUuid) throws HttpException, URISyntaxException {
+		if (tempConceptUuid == null) return;
+		restfulClient.deleteEntity(new URI(openmrsUrl + CONCEPT_PATH + "/" + tempConceptUuid + "?purge"));
+	}
+	
+
+	private void deleteUser(MRSUser creator) throws HttpException, URISyntaxException {
+		if(creator == null) return;
+		restfulClient.deleteEntity(new URI(openmrsUrl + "/ws/rest/v1/user/" + creator.getId() + "?purge"));
+		restfulClient.deleteEntity(new URI(openmrsUrl + "/ws/rest/v1/person/" + creator.getPerson().getId() + "?purge"));
+	}
+}
