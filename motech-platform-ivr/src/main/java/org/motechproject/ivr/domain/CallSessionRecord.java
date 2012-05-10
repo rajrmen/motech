9886135455d9af5a5b477553731f58f4848e4f0b
Vignesh R E <vignesh.littlemaster@gmail.com>
2012-05-10 18:15:16 +0530

    V2, Katta | #9 | Introduced call session record data object

---
+++ motech-platform-ivr/src/main/java/org/motechproject/ivr/domain/CallSessionRecord.java
@@ -0,0 +1,53 @@
+package org.motechproject.ivr.domain;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.ektorp.support.TypeDiscriminator;
+import org.hamcrest.Matcher;
+import org.motechproject.model.MotechBaseDataObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CallSessionRecord extends MotechBaseDataObject {
+
+    @JsonProperty
+    private Map<String, String> data;
+
+    @JsonProperty
+    @TypeDiscriminator
+    private String sessionId;
+
+    CallSessionRecord() {
+    }
+
+    public CallSessionRecord(String sessionId) {
+        this.sessionId = sessionId;
+        data = new HashMap<String, String>();
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CallSessionRecord that = (CallSessionRecord) o;
+
+        return new EqualsBuilder().append(this.sessionId, that.sessionId).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(sessionId).toHashCode();
+    }
+
+    public void add(String key, String value) {
+        data.put(key, value);
+    }
+
+    public String valueFor(String key) {
+        return data.get(key);
+    }
+}
