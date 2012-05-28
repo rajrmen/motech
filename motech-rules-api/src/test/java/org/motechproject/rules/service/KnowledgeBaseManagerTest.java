b2b12a28dce97d46c48eb6035bf3b4908c4e0a2a
Sanchit Bahal <sanchit.bahal@live.com>
2012-05-28 17:24:32 +0530

    Banka,Sanchit | #25 | Moved out rule engine to its own module.

--- motech-rules-api/src/test/java/org/motechproject/rules/service/KnowledgeBaseManagerTest.java
+++ motech-rules-api/src/test/java/org/motechproject/rules/service/KnowledgeBaseManagerTest.java
@@ -29,18 +29,48 @@
  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
  * OF SUCH DAMAGE.
  */
-package org.motechproject.dao;
+package org.motechproject.rules.service;
 
-import org.ektorp.CouchDbConnector;
-import org.motechproject.model.Rule;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Repository;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
-@Repository
-public class AllRules extends MotechBaseRepository<Rule> {
-    @Autowired
-    public AllRules(@Qualifier("ruleDatabase") CouchDbConnector db) {
-        super(Rule.class, db);
+import java.io.File;
+import java.net.URLDecoder;
+
+import org.motechproject.rules.domain.Rule;
+import org.drools.KnowledgeBase;
+import org.drools.runtime.StatelessKnowledgeSession;
+import org.junit.Test;
+import org.motechproject.rules.repository.AllRules;
+
+
+public class KnowledgeBaseManagerTest {
+
+    @Test
+    public void addOrUpdateRuleTest() throws Exception {
+        String ruleFolder = "/rules";
+        String ruleFile = "test.drl";
+
+        KnowledgeBaseManager kbm = new KnowledgeBaseManager();
+        AllRules repo = mock(AllRules.class);
+        kbm.setAllRules(repo);
+
+        File file = new File(URLDecoder.decode(getClass().getResource(ruleFolder + "/" + ruleFile).getFile(), "UTF-8"));
+        kbm.addOrUpdateRule(file);
+
+        verify(repo).contains(ruleFile);
+        verify(repo).add(any(Rule.class));
+
+        KnowledgeBase kbase = kbm.getKnowledgeBase(ruleFile);
+        StatelessKnowledgeSession ksession = kbase.newStatelessKnowledgeSession();
+        Applicant applicant = new Applicant("Mr John Smith", 16);
+        assertTrue(applicant.isValid());
+        ksession.execute(applicant);
+        assertFalse(applicant.isValid());
     }
+
 }
+
