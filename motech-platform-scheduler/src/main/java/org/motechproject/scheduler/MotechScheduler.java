56d7e655f29a35c298e6e5d01152275837bf585a
Łukasz Lewczyński <llewczynski@soldevelo.com>
2012-06-18 15:29:04 +0200

    Revert "llewczynski | MotechScheduler class refactoring"
    This reverts commit 70a90532da997f8dfb928172047e1c187a89d572.

--- motech-platform-scheduler/src/main/java/org/motechproject/scheduler/MotechScheduler.java
+++ motech-platform-scheduler/src/main/java/org/motechproject/scheduler/MotechScheduler.java
@@ -29,8 +29,8 @@ public class MotechScheduler {
     private final static Logger log = LoggerFactory.getLogger(MotechSchedulerServiceImpl.class);
     private static final String CONFIG_LOCATION = "/applicationPlatformScheduler.xml";
 
-    private final static String SCHEDULE_TEST_INPUT_PARAM = "-st";
-    private final static String UNSCHEDULE_TEST_INPUT_PARAM = "-ust";
+    private final static String SCHEDULE_TEST_INPUT_PARAM = "-t";
+    private final static String UNSCHEDULE_TEST_INPUT_PARAM = "-c";
 
     private final static String EVENT_MESSAGE_INPUT_PARAM = "-e";
     private final static String CRON_SCHEDULABLE_JOB_INPUT_PARAM = "-csj";
@@ -47,7 +47,6 @@ public class MotechScheduler {
 
     private final static String TEST_EVENT_NAME = "testEvent";
     private static final String TEST_SUBJECT = "test";
-    private static final String TEST_CRON_EXPRESSION = "0/5 * * * * ?";
 
     @Autowired
     private MotechSchedulerService schedulerService;
@@ -55,7 +54,7 @@ public class MotechScheduler {
     @Autowired
     private SchedulerFireEventGateway schedulerFireEventGateway;
 
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         AbstractApplicationContext ctx = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
 
         // add a shutdown hook for the above context...
@@ -72,13 +71,58 @@ public class MotechScheduler {
                 } else if (UNSCHEDULE_TEST_INPUT_PARAM.equals(args[0])) {
                     motechScheduler.unscheduleTestEvent();
                 } else if (EVENT_MESSAGE_INPUT_PARAM.equals(args[0])) {
-                    motechScheduler.sendEventMessage(convertArguments(args));
+                    Map<String, String> map = motechScheduler.getParams(args);
+
+                    if (map.containsKey(SUBJECT) && map.containsKey(PARAMETERS)) {
+                        motechScheduler.sendEventMessage(map.get(SUBJECT),
+                                motechScheduler.getEventParameters(map.get(PARAMETERS)));
+                    } else if (map.containsKey(SUBJECT)) {
+                        motechScheduler.sendEventMessage(map.get(SUBJECT), null);
+                    } else {
+                        log.info(String.format("Usage: java MotechScheduler %s %s [%s]", EVENT_MESSAGE_INPUT_PARAM,
+                                SUBJECT, PARAMETERS));
+                    }
                 } else if (CRON_SCHEDULABLE_JOB_INPUT_PARAM.equals(args[0])) {
-                    motechScheduler.scheduleCronSchedulableJob(convertArguments(args));
+                    Map<String, String> map = motechScheduler.getParams(args);
+                    Date startTime = map.containsKey(START_DATE) ? date(map.get(START_DATE)) : null;
+                    Date endTime = map.containsKey(END_DATE) ? date(map.get(END_DATE)) : null;
+
+                    if (map.containsKey(SUBJECT) && map.containsKey(CRON_EXPRESSION)) {
+                        motechScheduler.scheduleCronSchedulableJob(map.get(SUBJECT),
+                                motechScheduler.getEventParameters(map.get(PARAMETERS)),
+                                map.get(CRON_EXPRESSION), startTime, endTime);
+                    } else {
+                        log.info(String.format("Usage: java MotechScheduler %s %s [%s] %s [%s] [%s]",
+                                CRON_SCHEDULABLE_JOB_INPUT_PARAM, SUBJECT, PARAMETERS, CRON_EXPRESSION, START_DATE,
+                                END_DATE));
+                    }
                 } else if (REPEATING_SCHEDULABLE_JOB_INPUT_PARAM.equals(args[0])) {
-                    motechScheduler.scheduleRepeatingSchedulableJob(convertArguments(args));
+                    Map<String, String> map = motechScheduler.getParams(args);
+                    Date endTime = map.containsKey(END_DATE) ? date(map.get(END_DATE)) : null;
+                    Integer repeatCount = map.containsKey(REPEAT_COUNT) ? Integer.valueOf(map.get(REPEAT_COUNT)) : null;
+                    Long repeatInterval = map.containsKey(REPEAT_INTERVAL) ? Long.valueOf(map.get(REPEAT_INTERVAL)) : null;
+
+                    if (map.containsKey(SUBJECT) && map.containsKey(START_DATE) &&
+                            (repeatCount != null || repeatInterval != null)) {
+                        motechScheduler.scheduleRepeatingSchedulableJob(map.get(SUBJECT),
+                                motechScheduler.getEventParameters(map.get(PARAMETERS)), date(map.get(START_DATE)),
+                                endTime, repeatCount, repeatInterval);
+                    } else {
+                        log.info(String.format("Usage: java MotechScheduler %s %s [%s] %s [%s] [%s] %s",
+                                REPEATING_SCHEDULABLE_JOB_INPUT_PARAM, SUBJECT, PARAMETERS, START_DATE, END_DATE,
+                                REPEAT_COUNT, REPEAT_INTERVAL));
+                    }
                 } else if (RUN_ONCE_SCHEDULABLE_JOB_INPUT_PARAM.equals(args[0])) {
-                    motechScheduler.scheduleRunOnceSchedulableJob(convertArguments(args));
+                    Map<String, String> map = motechScheduler.getParams(args);
+                    Date startDate = map.containsKey(START_DATE) ? date(map.get(START_DATE)) : null;
+
+                    if (map.containsKey(SUBJECT) && startDate != null) {
+                        motechScheduler.scheduleRunOnceSchedulableJob(map.get(SUBJECT),
+                                motechScheduler.getEventParameters(map.get(PARAMETERS)), startDate);
+                    } else {
+                        log.info(String.format("Usage: java MotechScheduler %s %s [%s] %s ",
+                                CRON_SCHEDULABLE_JOB_INPUT_PARAM, SUBJECT, PARAMETERS, START_DATE));
+                    }
                 } else {
                     log.warn(String.format("Unknown parameter: %s - ignored", args[0]));
                 }
@@ -88,95 +132,59 @@ public class MotechScheduler {
         }
     }
 
-    private static Map<String, String> convertArguments(final String[] args) {
-        Map<String, String> params = new HashMap<String, String>(args.length - 1);
+    private void sendEventMessage(final String subject, final Map<String, Object> parameters) {
+        MotechEvent event = new MotechEvent(subject, parameters);
 
-        for (int i = 1; i < args.length; i += 2) {
-            params.put(args[i], args[i + 1]);
-        }
+        schedulerFireEventGateway.sendEventMessage(event);
 
-        return params;
+        log.info(String.format("Sending Motech Event Message: %s", event));
     }
 
-    private void sendEventMessage(final Map<String, String> map) {
-        if (map.containsKey(SUBJECT)) {
-            MotechEvent event = createMotechEvent(map);
-            schedulerFireEventGateway.sendEventMessage(createMotechEvent(map));
+    private void scheduleCronSchedulableJob(final String subject, final Map<String, Object> parameters,
+                                            final String cronExpression, final Date startTime, final Date endTime) {
+        MotechEvent motechEvent = new MotechEvent(subject, parameters);
+        CronSchedulableJob cronSchedulableJob = new CronSchedulableJob(motechEvent, cronExpression, startTime, endTime);
 
-            log.info(String.format("Sending Motech Event Message: %s", event));
-        } else {
-            log.info(String.format("Usage: java MotechScheduler %s %s [%s]",
-                    EVENT_MESSAGE_INPUT_PARAM, SUBJECT, PARAMETERS));
-        }
-    }
-
-    private void scheduleCronSchedulableJob(final Map<String, String> map) {
-        Date startTime = map.containsKey(START_DATE) ? date(map.get(START_DATE)) : null;
-        Date endTime = map.containsKey(END_DATE) ? date(map.get(END_DATE)) : null;
-
-        if (map.containsKey(SUBJECT) && map.containsKey(CRON_EXPRESSION)) {
-            CronSchedulableJob cronSchedulableJob = new CronSchedulableJob(createMotechEvent(map),
-                    map.get(CRON_EXPRESSION), startTime, endTime);
-
-            try {
-                log.info(String.format("Scheduling job: %s", cronSchedulableJob));
-                schedulerService.safeScheduleJob(cronSchedulableJob);
-            } catch (Exception e) {
-                log.warn(String.format("Can not schedule job. %s", e.getMessage()));
-            }
-        } else {
-            log.info(String.format("Usage: java MotechScheduler %s %s [%s] %s [%s] [%s]",
-                    CRON_SCHEDULABLE_JOB_INPUT_PARAM, SUBJECT, PARAMETERS, CRON_EXPRESSION, START_DATE, END_DATE));
+        try {
+            log.info(String.format("Scheduling job: %s", cronSchedulableJob));
+            schedulerService.safeScheduleJob(cronSchedulableJob);
+        } catch (Exception e) {
+            log.warn(String.format("Can not schedule job. %s", e.getMessage()));
         }
     }
 
-    private void scheduleRepeatingSchedulableJob(final Map<String, String> map) {
-        Date endTime = map.containsKey(END_DATE) ? date(map.get(END_DATE)) : null;
-        Integer repeatCount = map.containsKey(REPEAT_COUNT) ? Integer.valueOf(map.get(REPEAT_COUNT)) : null;
-        Long repeatInterval = map.containsKey(REPEAT_INTERVAL) ? Long.valueOf(map.get(REPEAT_INTERVAL)) : null;
+    private void scheduleRepeatingSchedulableJob(final String subject, final Map<String, Object> parameters,
+                                                 final Date startTime, final Date endTime, final Integer repeatCount,
+                                                 final Long repeatIntervalInMilliSeconds) {
+        MotechEvent motechEvent = new MotechEvent(subject, parameters);
+        RepeatingSchedulableJob repeatingSchedulableJob = new RepeatingSchedulableJob(motechEvent, startTime, endTime, repeatCount, repeatIntervalInMilliSeconds);
 
-        if (map.containsKey(SUBJECT) && map.containsKey(START_DATE) && (repeatCount != null || repeatInterval != null)) {
-            RepeatingSchedulableJob repeatingSchedulableJob = new RepeatingSchedulableJob(createMotechEvent(map),
-                    date(map.get(START_DATE)), endTime, repeatCount, repeatInterval);
-
-            try {
-                log.info(String.format("Scheduling job: %s", repeatingSchedulableJob));
-                schedulerService.safeScheduleRepeatingJob(repeatingSchedulableJob);
-            } catch (Exception e) {
-                log.warn(String.format("Can not schedule job. %s", e.getMessage()));
-            }
-        } else {
-            log.info(String.format("Usage: java MotechScheduler %s %s [%s] %s [%s] [%s] %s",
-                    REPEATING_SCHEDULABLE_JOB_INPUT_PARAM, SUBJECT, PARAMETERS, START_DATE, END_DATE, REPEAT_COUNT,
-                    REPEAT_INTERVAL));
+        try {
+            log.info(String.format("Scheduling job: %s", repeatingSchedulableJob));
+            schedulerService.safeScheduleRepeatingJob(repeatingSchedulableJob);
+        } catch (Exception e) {
+            log.warn(String.format("Can not schedule job. %s", e.getMessage()));
         }
     }
 
-    private void scheduleRunOnceSchedulableJob(final Map<String, String> map) {
-        Date startDate = map.containsKey(START_DATE) ? date(map.get(START_DATE)) : null;
+    private void scheduleRunOnceSchedulableJob(final String subject, final Map<String, Object> parameters,
+                                               final Date startDate) {
+        MotechEvent motechEvent = new MotechEvent(subject, parameters);
+        RunOnceSchedulableJob runOnceSchedulableJob = new RunOnceSchedulableJob(motechEvent, startDate);
 
-        if (map.containsKey(SUBJECT) && startDate != null) {
-            RunOnceSchedulableJob runOnceSchedulableJob = new RunOnceSchedulableJob(createMotechEvent(map), startDate);
-
-            try {
-                log.info(String.format("Scheduling job: %s", runOnceSchedulableJob));
-                schedulerService.safeScheduleRunOnceJob(runOnceSchedulableJob);
-            } catch (Exception e) {
-                log.warn(String.format("Can not schedule job. %s", e.getMessage()));
-            }
-        } else {
-            log.info(String.format("Usage: java MotechScheduler %s %s [%s] %s ", CRON_SCHEDULABLE_JOB_INPUT_PARAM,
-                    SUBJECT, PARAMETERS, START_DATE));
+        try {
+            log.info(String.format("Scheduling job: %s", runOnceSchedulableJob));
+            schedulerService.safeScheduleRunOnceJob(runOnceSchedulableJob);
+        } catch (Exception e) {
+            log.warn(String.format("Can not schedule job. %s", e.getMessage()));
         }
     }
 
     private void scheduleTestEvent() {
-        Map<String, String> map = new HashMap<String, String>(3);
-        map.put(SUBJECT, TEST_SUBJECT);
-        map.put(PARAMETERS, String.format("{ %s: '%s' }", MotechSchedulerService.JOB_ID_KEY, TEST_EVENT_NAME));
-        map.put(CRON_EXPRESSION, TEST_CRON_EXPRESSION);
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put(MotechSchedulerService.JOB_ID_KEY, TEST_EVENT_NAME);
 
-        scheduleCronSchedulableJob(map);
+        scheduleCronSchedulableJob(SUBJECT, params, "0/5 * * * * ?", null, null);
     }
 
     private void unscheduleTestEvent() {
@@ -188,6 +196,16 @@ public class MotechScheduler {
         }
     }
 
+    private Map<String, String> getParams(final String[] args) {
+        Map<String, String> params = new HashMap<String, String>(args.length - 1);
+
+        for (int i = 1; i < args.length; i += 2) {
+            params.put(args[i], args[i + 1]);
+        }
+
+        return params;
+    }
+
     private Map<String, Object> getEventParameters(final String parametersAsJSON) {
         Map<String, Object> map = new HashMap<String, Object>();
         MotechJsonReader reader = new MotechJsonReader();
@@ -196,22 +214,9 @@ public class MotechScheduler {
 
         if (obj != null) {
             map.putAll((Map<String, Object>) obj);
-        } else {
-            map = null;
         }
 
         return map;
     }
 
-    private MotechEvent createMotechEvent(final Map<String, String> map) {
-        MotechEvent event = null;
-
-        if (map.containsKey(SUBJECT) && map.containsKey(PARAMETERS)) {
-            event = new MotechEvent(map.get(SUBJECT), getEventParameters(map.get(PARAMETERS)));
-        } else if (map.containsKey(SUBJECT)) {
-            event = new MotechEvent(map.get(SUBJECT));
-        }
-
-        return event;
-    }
 }
