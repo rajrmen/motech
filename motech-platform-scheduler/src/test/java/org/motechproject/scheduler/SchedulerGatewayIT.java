7cad854b215be3562d28e8ff21d00ecb7525c858
Łukasz Lewczyński <llewczynski@soldevelo.com>
2012-06-20 11:23:57 +0200

    llewczynski | #27 | Upgraded test for motech scheduler
    Change-Id: I51a2d428fb79386b9853f06d0e56f0517b47540f

--- motech-platform-scheduler/src/test/java/org/motechproject/scheduler/SchedulerGatewayIT.java
+++ motech-platform-scheduler/src/test/java/org/motechproject/scheduler/SchedulerGatewayIT.java
@@ -35,6 +35,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.motechproject.scheduler.domain.CronSchedulableJob;
 import org.motechproject.scheduler.domain.MotechEvent;
+import org.motechproject.scheduler.domain.RepeatingSchedulableJob;
 import org.motechproject.scheduler.domain.RunOnceSchedulableJob;
 import org.motechproject.scheduler.gateway.MotechSchedulerGateway;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,8 +50,7 @@ import java.util.Map;
  * Motech Scheduler integration test
  *
  * @author Igor (iopushnyev@2paths.com)
- * Date: 23/02/11
- *
+ *         Date: 23/02/11
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/testPlatformSchedulerApplicationContext.xml")
@@ -60,17 +60,29 @@ public class SchedulerGatewayIT {
 
     @Test
     public void testMotechScheduler() {
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("JobID", "test_1");
-        MotechEvent motechEvent = new MotechEvent("testEvent", params);
-        CronSchedulableJob cronSchedulableJob = new CronSchedulableJob(motechEvent, "0/5 0 * * * ?");
+        Map<String, Object> cronParams = new HashMap<String, Object>();
+        cronParams.put("JobID", "test_cron");
+
+        Map<String, Object> runOnceParams = new HashMap<String, Object>();
+        runOnceParams.put("JobID", "test_run_once");
+
+        Map<String, Object> repeatingParams = new HashMap<String, Object>();
+        repeatingParams.put("JobID", "test_repeating");
+
+        MotechEvent cronEvent = new MotechEvent("cronTestEvent", cronParams);
+        MotechEvent runOnceEvent = new MotechEvent("runOnceTestEvent", runOnceParams);
+        MotechEvent repeatingEvent = new MotechEvent("repeatingTestEvent", repeatingParams);
+
+        CronSchedulableJob cronSchedulableJob = new CronSchedulableJob(cronEvent, "0/5 * * * * ?");
+        RunOnceSchedulableJob runOnceSchedulableJob = new RunOnceSchedulableJob(runOnceEvent, new Date((new Date().getTime() + 5000)));
+        RepeatingSchedulableJob repeatingSchedulableJob = new RepeatingSchedulableJob(repeatingEvent, new Date(), null, 5, 5 * 1000L);
 
         motechSchedulerGateway.scheduleJob(cronSchedulableJob);
 
-        motechSchedulerGateway.unscheduleJob("test_1");
-        
-        RunOnceSchedulableJob runOnceSchedulableJob = new RunOnceSchedulableJob(motechEvent, new Date((new Date().getTime()+5000)));
-
         motechSchedulerGateway.scheduleRunOnceJob(runOnceSchedulableJob);
+
+        motechSchedulerGateway.scheduleRepeatingJob(repeatingSchedulableJob);
+
+        motechSchedulerGateway.unscheduleJob("test_cron");
     }
 }
