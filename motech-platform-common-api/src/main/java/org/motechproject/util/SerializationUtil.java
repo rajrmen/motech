7c7eea6dacf2ad96ca73871b2cc606aa09d69bbf
Soham Ghosh <sohamghosh@gmail.com>
2012-07-25 20:11:03 +0530

    OSGfied all depencies of platform-common-api
    Change-Id: I0f7d4921ddcb3694c4160d35d9562ed8e9047221

--- motech-platform-common-api/src/main/java/org/motechproject/util/SerializationUtil.java
+++ motech-platform-common-api/src/main/java/org/motechproject/util/SerializationUtil.java
@@ -1,30 +1,21 @@
 package org.motechproject.util;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.SerializationUtils;
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
-import java.io.IOException;
 import java.io.Serializable;
 
 public class SerializationUtil {
-    static private BASE64Encoder encode = new BASE64Encoder();
-    static private BASE64Decoder decode = new BASE64Decoder();
+    static private Base64 codec = new Base64();
 
     public static String toString(Serializable obj) {
         if (obj == null) return null;
 
         byte[] bytes = SerializationUtils.serialize(obj);
-        return encode.encode(bytes);
+        return new String(codec.encode(bytes));
     }
 
     public static Object toObject(String str) {
-        Object out = null;
-        try {
-            return SerializationUtils.deserialize(decode.decodeBuffer(str));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return out;
+        return SerializationUtils.deserialize(codec.decode(str.getBytes()));
     }
 }
