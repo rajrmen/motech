1a7fc0f7e3ff26bb0d15ced87bde53c2a7c408c9
Manohar Akula <manohar.akula@gmail.com>
2012-08-03 17:00:38 +0530

    introducing functional test profile for build and jvmfaketime for writing time dependent tests
    Change-Id: I08ee0f2f49fbefea81a01bec671631b6908e21eb

---
+++ motech-testing-utils/src/main/java/org/motechproject/testing/utils/ft/JvmFakeTime.java
@@ -0,0 +1,17 @@
+package org.motechproject.testing.utils.ft;
+
+import java.lang.reflect.Method;
+
+public abstract class JvmFakeTime {
+
+    public static void load() {
+        try {
+            Method m = ClassLoader.class.getDeclaredMethod("loadLibrary", Class.class, String.class, Boolean.TYPE);
+            m.setAccessible(true);
+            m.invoke(null, System.class, "jvmfaketime", false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
