d790e60370f6ea095c8b05094f2caa46a2ecaab4
Łukasz Lewczyński <llewczynski@soldevelo.com>
2013-01-09 10:53:21 +0100

    llewczynski | Added new TaskBundleIT test
    Change-Id: I3695ec1527d30d71b19649f17ece871bcb2fa6ee

--- modules/tasks/tasks-bundle/src/test/java/org/motechproject/tasks/osgi/TasksBundleIT.java
+++ modules/tasks/tasks-bundle/src/test/java/org/motechproject/tasks/osgi/TasksBundleIT.java
@@ -4,8 +4,11 @@ import org.motechproject.event.listener.EventListenerRegistryService;
 import org.motechproject.event.listener.EventRelay;
 import org.motechproject.server.config.service.PlatformSettingsService;
 import org.motechproject.tasks.domain.Channel;
+import org.motechproject.tasks.domain.DataProvider;
 import org.motechproject.tasks.repository.AllChannels;
+import org.motechproject.tasks.repository.AllDataProviders;
 import org.motechproject.tasks.service.ChannelService;
+import org.motechproject.tasks.service.DataProviderService;
 import org.motechproject.testing.osgi.BaseOsgiIT;
 import org.osgi.framework.ServiceReference;
 
@@ -16,33 +19,22 @@ import static java.util.Arrays.asList;
 public class TasksBundleIT extends BaseOsgiIT {
     private static final Integer TRIES_COUNT = 50;
 
-    public void testTasksService() throws InterruptedException {
+    public void testCoreServiceReferences() {
         assertNotNull(bundleContext.getServiceReference(PlatformSettingsService.class.getName()));
         assertNotNull(bundleContext.getServiceReference(EventListenerRegistryService.class.getName()));
         assertNotNull(bundleContext.getServiceReference(EventRelay.class.getName()));
+    }
 
-        ServiceReference channelServiceOsgi;
-        int channelServiceOsgiTries = 0;
-
-        do {
-            channelServiceOsgi = bundleContext.getServiceReference(ChannelService.class.getName());
-            ++channelServiceOsgiTries;
-            Thread.sleep(500);
-        } while (channelServiceOsgi == null && channelServiceOsgiTries < TRIES_COUNT);
-
-        assertNotNull(channelServiceOsgi);
-
-        ChannelService channelService = (ChannelService) bundleContext.getService(channelServiceOsgi);
-        assertNotNull(channelService);
-
+    public void testChannelService() throws InterruptedException {
+        ChannelService channelService = getService(ChannelService.class);
         Channel fromFile;
-        int channelTries = 0;
+        int tries = 0;
 
         do {
             fromFile = channelService.getChannel("test", "test", "0.15");
-            ++channelTries;
+            ++tries;
             Thread.sleep(500);
-        } while (fromFile == null && channelTries < TRIES_COUNT);
+        } while (fromFile == null && tries < TRIES_COUNT);
 
         assertNotNull(fromFile);
 
@@ -58,14 +50,62 @@ public class TasksBundleIT extends BaseOsgiIT {
         assertNull(fromDB);
     }
 
+    public void testDataProviderService() throws InterruptedException {
+        DataProviderService dataProviderService = getService(DataProviderService.class);
+        DataProvider fromFile;
+        int tries = 0;
+
+        do {
+            fromFile = dataProviderService.getProvider("MRS");
+            ++tries;
+            Thread.sleep(500);
+        } while (fromFile == null && tries < TRIES_COUNT);
+
+        assertNotNull(fromFile);
+
+        AllDataProviders allDataProviders = getApplicationContext().getBean(AllDataProviders.class);
+        DataProvider fromDB = allDataProviders.byName("MRS");
+
+        assertNotNull(fromDB);
+        assertEquals(fromDB, fromFile);
+
+        allDataProviders.remove(fromDB);
+
+        fromDB = allDataProviders.byName("MRS");
+        assertNull(fromDB);
+    }
+
     @Override
     protected List<String> getImports() {
-        return asList("org.motechproject.tasks.util", "org.motechproject.server.config");
+        return asList("org.motechproject.tasks.util", "org.motechproject.server.config", "org.motechproject.commons.api");
     }
 
     @Override
     protected String[] getConfigLocations() {
         return new String[]{"/META-INF/osgi/testApplicationTasksBundle.xml"};
+    }
+
+    private <T> T getService(Class<T> clazz) throws InterruptedException {
+        T service = clazz.cast(bundleContext.getService(getServiceReference(clazz)));
+
+        assertNotNull(service);
+
+        return service;
+    }
+
+    private <T> ServiceReference getServiceReference(Class<T> clazz) throws InterruptedException {
+        ServiceReference serviceReference;
+        int tries = 0;
+
+        do {
+            serviceReference = bundleContext.getServiceReference(clazz.getName());
+            ++tries;
+            Thread.sleep(500);
+        } while (serviceReference == null && tries < TRIES_COUNT);
+
+        assertNotNull(serviceReference);
+
+        return serviceReference;
     }
 
 }
