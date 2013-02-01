a0d8ea382f7345912f0b76ff049a480df9527be8
Paweł Gesek <pgesek@soldevelo.com>
2013-02-01 13:40:40 +0100

    Revert "pgesek | Testing RequestContextFilter"
    This reverts commit ca5584bd14c7f1c602b331b024ee88a429e9ed2d.

--- platform/tenant/src/main/java/org/motechproject/tenant/service/TenantRepositoryService.java
+++
@@ -1,8 +0,0 @@
-package org.motechproject.tenant.service;
-
-import org.springframework.web.context.request.RequestAttributes;
-
-public interface TenantRepositoryService {
-
-    public String getTenantId(RequestAttributes requestAttributes);
-}
--- platform/web-security/web-security/src/main/java/org/motechproject/security/authentication/MotechAuthenticationProvider.java
+++ platform/web-security/web-security/src/main/java/org/motechproject/security/authentication/MotechAuthenticationProvider.java
@@ -6,7 +6,6 @@ import org.motechproject.security.helper.SecurityHelper;
 import org.motechproject.security.repository.AllMotechRoles;
 import org.motechproject.security.repository.AllMotechUsers;
 import org.motechproject.security.service.MotechUserProfile;
-import org.motechproject.tenant.service.TenantRepositoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,11 +13,6 @@ import org.springframework.security.authentication.dao.AbstractUserDetailsAuthen
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class MotechAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
@@ -30,9 +24,6 @@ public class MotechAuthenticationProvider extends AbstractUserDetailsAuthenticat
     private AllMotechUsers allMotechUsers;
     private MotechPasswordEncoder passwordEncoder;
     private AllMotechRoles allMotechRoles;
-
-    @Autowired
-    private TenantRepositoryService tenantRepositoryService;
 
     @Autowired
     public MotechAuthenticationProvider(AllMotechUsers allMotechUsers, MotechPasswordEncoder motechPasswordEncoder, AllMotechRoles allMotechRoles) {
@@ -54,13 +45,7 @@ public class MotechAuthenticationProvider extends AbstractUserDetailsAuthenticat
 
     @Override
     protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) {
-        RequestAttributes request =  RequestContextHolder.currentRequestAttributes();
-        String tenantId = tenantRepositoryService.getTenantId(request);
-
-        // we can use the tenant ID here to retrieve the user
-
         MotechUser user = allMotechUsers.findByUserName(username);
-
         if (user == null) {
             throw new BadCredentialsException(USER_NOT_FOUND);
         } else if (!user.isActive()) {
@@ -70,4 +55,5 @@ public class MotechAuthenticationProvider extends AbstractUserDetailsAuthenticat
             return new User(user.getUserName(), user.getPassword(), user.isActive(), true, true, true, SecurityHelper.getAuthorities(user.getRoles(), allMotechRoles));
         }
     }
+
 }
