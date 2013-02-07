ac0a065287de7dd329808b6b14a879cbf1f8abda
wstrzelczyk <wstrzelczyk@soldevelo.com>
2013-02-07 13:21:21 +0100

    W.Strzelczyk | #532 | MRS UI panel - added pagination, search mechanism
    Change-Id: Ib306aa8f9bd333c36fec8f5b4f2f4366a434fa3c

--- modules/mrs/api-bundle/src/main/resources/webapp/js/controllers.js
+++ modules/mrs/api-bundle/src/main/resources/webapp/js/controllers.js
@@ -5,6 +5,72 @@
 function DashboardCtrl($scope, Patients, $http) {
 
       //$scope.patients = Patents.query();
+    $scope.filteredItems = [];
+    $scope.groupedItems = [];
+    $scope.itemsPerPage = 10;
+    $scope.pagedItems = [];
+    $scope.currentPage = 0;
+
+    var searchMatch = function (item, searchQuery) {
+        if (!searchQuery) {
+            return true;
+        }
+        return item.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1;
+    };
+
+    $scope.search = function () {
+
+        $scope.filteredItems = $filter('filter')($scope.allPatients, function (item) {
+            for(var attr in item) {
+                if (searchMatch(item[attr], $scope.query))
+                    return true;
+            }
+            return false;
+        });
+        $scope.currentPage = 0;
+        $scope.groupToPages();
+    };
+
+    $scope.groupToPages = function () {
+        $scope.pagedItems = [];
+
+        for (var i = 0; i < $scope.filteredItems.length; i++) {
+            if (i % $scope.itemsPerPage === 0) {
+                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [ $scope.filteredItems[i] ];
+            } else {
+                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.filteredItems[i]);
+            }
+        }
+    };
+
+    $scope.range = function (start, end) {
+        var ret = [];
+        if (!end) {
+            end = start;
+            start = 0;
+        }
+        for (var i = start; i < end; i++) {
+            ret.push(i);
+        }
+        return ret;
+    };
+
+    $scope.prevPage = function () {
+        if ($scope.currentPage > 0) {
+            $scope.currentPage--;
+        }
+    };
+
+    $scope.nextPage = function () {
+        if ($scope.currentPage < $scope.pagedItems.length - 1) {
+            $scope.currentPage++;
+        }
+    };
+
+    $scope.setPage = function () {
+        $scope.currentPage = this.number;
+    };
+
 }
 
 function ManageMrsCtrl($scope, $routeParams, $http) {
