f8a169e7a947e8167eb22400dddd76a732699e98
wstrzelczyk <wstrzelczyk@soldevelo.com>
2013-02-07 11:41:12 +0100

    W.Strzelczyk | #532 | MRS UI panel - part1
    Change-Id: Ia288a67f79b06887822f2a91fde6da4cb03377ac

---
+++ modules/mrs/api-bundle/src/main/resources/webapp/js/controllers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* Controllers */
+
+function DashboardCtrl($scope, Patients, $http) {
+
+      //$scope.patients = Patents.query();
+}
+
+function ManageMrsCtrl($scope, $routeParams, $http) {
+
+
+}
