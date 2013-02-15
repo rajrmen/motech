d5578c3d9f67ed14655e7ce810c15e25d447be35
Paweł Gesek <pgesek@soldevelo.com>
2013-02-15 16:54:21 +0100

    pgesek | Revert some unintended changes
    Change-Id: I0ea4f7d5770bfdb5b41647e6f8d7bfd9632feb28

--- modules/message-campaign/message-campaign/src/main/java/org/motechproject/server/messagecampaign/domain/campaign/AbsoluteCampaign.java
+++ modules/message-campaign/message-campaign/src/main/java/org/motechproject/server/messagecampaign/domain/campaign/AbsoluteCampaign.java
@@ -1,8 +1,6 @@
 package org.motechproject.server.messagecampaign.domain.campaign;
 
-import org.codehaus.jackson.annotate.JsonTypeInfo;
 import org.motechproject.server.messagecampaign.domain.message.AbsoluteCampaignMessage;
 
-@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@class")
 public class AbsoluteCampaign extends Campaign<AbsoluteCampaignMessage> {
 }
