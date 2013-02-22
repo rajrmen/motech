c02ccf1d15085ecc22b5c91e9260aceb88ef1c33
Jakub Wierzbowski <jwierzbowski@soldevelo.com>
2013-02-22 18:14:28 +0100

    jwierzbowski|#532|Added_motechID_Validation
    Change-Id: I1510166698265f65065c915bee61007c6726ee27

--- modules/mrs/api-bundle/src/main/resources/webapp/js/app.js
+++ modules/mrs/api-bundle/src/main/resources/webapp/js/app.js
@@ -5,11 +5,11 @@
 angular.module('motech-mrs', ['motech-dashboard', 'patientsServices', 'patientService', 'ngCookies', 'bootstrap', 'motech-widgets']).config(['$routeProvider',
     function ($routeProvider) {
         $routeProvider.
-            when('/dashboard', {templateUrl: '../mrs/partials/patients.html', controller: DashboardMrsCtrl}).
-            when('/dashboard/:mrsId', {templateUrl: '../mrs/partials/patients.html', controller: DashboardMrsCtrl}).
-            when('/mrs/new', {templateUrl: '../mrs/partials/form.html', controller: ManageMrsCtrl}).
-            when('/mrs/:mrsId/edit', {templateUrl: '../mrs/partials/form.html', controller: ManageMrsCtrl}).
-            when('/mrs/:mrsId/editAttributes', {templateUrl: '../mrs/partials/attributes.html', controller: ManageMrsCtrl}).
+            when('/dashboard', {templateUrl: '../mrs/partials/patients.html', controller: PatientMrsCtrl}).
+            when('/dashboard/:mrsId', {templateUrl: '../mrs/partials/patients.html', controller: PatientMrsCtrl}).
+            when('/mrs/new', {templateUrl: '../mrs/partials/form.html', controller: ManagePatientMrsCtrl}).
+            when('/mrs/:mrsId/edit', {templateUrl: '../mrs/partials/form.html', controller: ManagePatientMrsCtrl}).
+            when('/mrs/:mrsId/editAttributes', {templateUrl: '../mrs/partials/attributes.html', controller: ManagePatientMrsCtrl}).
             otherwise({redirectTo: '/dashboard'});
     }
 ]);
