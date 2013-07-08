0aaa8fe5308ca775adc7d55fdd1176e509852e3e
Łukasz Lewczyński <llewczynski@soldevelo.com>
2013-07-08 11:19:56 +0200

    changed $routeProvider to $stateProvider in email module
    Change-Id: Ibea88d8d9ddb43988e6dcf0f6955d4b5d1b79fc6

--- platform/email/src/main/resources/webapp/js/app.js
+++ platform/email/src/main/resources/webapp/js/app.js
@@ -3,11 +3,12 @@
 
     /* App Module */
 
-    angular.module('motech-email', ['motech-dashboard', 'ngCookies', 'bootstrap', 'sendEmailService', 'settingsService']).config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider.
-                when('/send', {templateUrl: '../email/resources/partials/sendEmail.html', controller: 'SendEmailController'}).
-                when('/settings', {templateUrl: '../email/resources/partials/settings.html', controller: 'SettingsController'}).
-                otherwise({redirectTo: '/send'});
+    angular.module('motech-email', ['motech-dashboard', 'ngCookies', 'bootstrap', 'sendEmailService', 'settingsService']).config(['$stateProvider', '$urlRouterProvider',
+        function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('send', {url: '/send', templateUrl: '../email/resources/partials/sendEmail.html', controller: 'SendEmailController'})
+                .state('moduleSettings', {url: '/settings', templateUrl: '../email/resources/partials/settings.html', controller: 'SettingsController'});
+
+            $urlRouterProvider.otherwise("/send");
     }]);
 }());
