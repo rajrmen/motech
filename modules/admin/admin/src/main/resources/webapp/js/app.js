cf887cce3f616dc0046e75d20e1d4d1fcbe2d8ec
Łukasz Lewczyński <llewczynski@soldevelo.com>
2013-07-08 11:25:22 +0200

    changed $routeProvider to $stateProvider in admin module
    Change-Id: I69d49bee8c6f7ce9a02b853b965aeba6bc30d7e8

--- modules/admin/admin/src/main/resources/webapp/js/app.js
+++ modules/admin/admin/src/main/resources/webapp/js/app.js
@@ -5,21 +5,22 @@
 
     angular.module('motech-admin', ['motech-dashboard', 'bundleServices', 'messageServices', 'platformSettingsServices',
         'moduleSettingsServices', 'logService', 'ngCookies', 'bootstrap', "notificationRuleServices", "notificationRuleDtoServices"])
-        .config(['$routeProvider', function($routeProvider) {
-          $routeProvider.
-              when('/bundles', {templateUrl: '../admin/partials/bundles.html', controller: 'BundleListCtrl'}).
-              when('/messages', {templateUrl: '../admin/partials/messages.html', controller: 'StatusMsgCtrl'}).
-              when('/settings', {templateUrl: '../admin/partials/settings.html', controller: 'SettingsCtrl'}).
-              when('/bundle/:bundleId', {templateUrl: '../admin/partials/bundle.html', controller: 'ModuleCtrl'}).
-              when('/bundleSettings/:bundleId', {templateUrl: '../admin/partials/bundleSettings.html', controller: 'BundleSettingsCtrl'}).
-              when('/modulePanels', {templateUrl: '../admin/partials/modulePanels.html'}).
-              when('/operations', {templateUrl: '../admin/partials/operations.html', controller: 'OperationsCtrl'}).
-              when('/log', {templateUrl: '../admin/partials/log.html', controller: 'ServerLogCtrl'}).
-              when('/queues', {templateUrl: '../admin/partials/queue_stats.html', controller: 'QueueStatisticsCtrl'}).
-              when('/queues/browse', {templateUrl: '../admin/partials/queue_message_stats.html', controller: 'MessageStatisticsCtrl'}).
-              when('/logOptions', {templateUrl: '../admin/partials/logOptions.html', controller: 'ServerLogOptionsCtrl'}).
-              when('/notificationRules', {templateUrl: '../admin/partials/notificationRules.html', controller: 'NotificationRuleCtrl'}).
-              otherwise({redirectTo: '/bundles'});
+        .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+          $stateProvider
+              .state('bundles', {url: '/bundles', templateUrl: '../admin/partials/bundles.html', controller: 'BundleListCtrl'})
+              .state('messages', {url: '/messages', templateUrl: '../admin/partials/messages.html', controller: 'StatusMsgCtrl'})
+              .state('settings', {url: '/settings', templateUrl: '../admin/partials/settings.html', controller: 'SettingsCtrl'})
+              .state('bundle', {url: '/bundle/:bundleId', templateUrl: '../admin/partials/bundle.html', controller: 'ModuleCtrl'})
+              .state('bundleSettings', {url: '/bundleSettings/:bundleId', templateUrl: '../admin/partials/bundleSettings.html', controller: 'BundleSettingsCtrl'})
+              .state('modulePanels', {url: '/modulePanels', templateUrl: '../admin/partials/modulePanels.html'})
+              .state('operations', {url: '/operations', templateUrl: '../admin/partials/operations.html', controller: 'OperationsCtrl'})
+              .state('log', {url: '/log', templateUrl: '../admin/partials/log.html', controller: 'ServerLogCtrl'})
+              .state('queues', {url: '/queues', templateUrl: '../admin/partials/queue_stats.html', controller: 'QueueStatisticsCtrl'})
+              .state('queuesBrowse', {url: '/queues/browse', templateUrl: '../admin/partials/queue_message_stats.html', controller: 'MessageStatisticsCtrl'})
+              .state('logOptions', {url: '/logOptions', templateUrl: '../admin/partials/logOptions.html', controller: 'ServerLogOptionsCtrl'})
+              .state('notificationRules', {url: '/notificationRules', templateUrl: '../admin/partials/notificationRules.html', controller: 'NotificationRuleCtrl'});
+
+          $urlRouterProvider.otherwise("/bundles");
     }]).filter('moduleName', function () {
         return function (input) {
             return input.replace(/(motech\s|\sapi|\sbundle)/ig, '');
