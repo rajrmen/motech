298853271538e46d2f80ac77395134de27e15e46
Paweł Gesek <pgesek@soldevelo.com>
2013-07-25 17:27:22 +0200

    Revert "Revert "Merge branch 'master' into dev/scheduler-ui""
    This reverts commit 1db3f3cbc58454c5b25867f357d630f293570723.

--- platform/email/src/main/java/org/motechproject/email/web/SendEmailController.java
+++ platform/email/src/main/java/org/motechproject/email/web/SendEmailController.java
@@ -1,6 +1,9 @@
 package org.motechproject.email.web;
 
+import org.motechproject.email.DeliveryStatus;
+import org.motechproject.email.domain.EmailRecord;
 import org.motechproject.email.model.Mail;
+import org.motechproject.email.service.EmailAuditService;
 import org.motechproject.email.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,19 +16,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.IOException;
+import java.util.Random;
+
+import static org.motechproject.commons.date.util.DateUtil.now;
 
 @Controller
 public class SendEmailController {
     private EmailSenderService senderService;
+    private EmailAuditService auditService;
 
     @Autowired
-    public SendEmailController(EmailSenderService senderService) {
+    public SendEmailController(EmailSenderService senderService, EmailAuditService auditService) {
         this.senderService = senderService;
+        this.auditService = auditService;
     }
+
+    private Random random = new Random();
 
     @RequestMapping(value = "/send", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
     public void sendEmail(@RequestBody Mail mail) {
+        auditService.log(new EmailRecord(
+                mail.getFromAddress(), mail.getToAddress(), mail.getSubject(), mail.getMessage(),
+                now(), DeliveryStatus.PENDING, Integer.toString(Math.abs(random.nextInt()))));
         senderService.send(mail);
     }
 
