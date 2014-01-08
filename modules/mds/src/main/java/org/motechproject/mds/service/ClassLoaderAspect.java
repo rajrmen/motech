8437ce90dc7d3943ddc2b8bddd21978df9913503
Łukasz Lewczyński <llewczynski@soldevelo.com>
2014-01-08 10:13:11 +0100

    fixed problem with spring aop
    Change-Id: I676ac556cb4f242be46c72f3cee72f9eb45a1a53

--- modules/mds/src/main/java/org/motechproject/mds/service/ClassLoaderAspect.java
+++ modules/mds/src/main/java/org/motechproject/mds/service/ClassLoaderAspect.java
@@ -3,8 +3,10 @@ package org.motechproject.mds.service;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class ClassLoaderAspect {
 
     @Around("within(org.motechproject.mds.service.impl.*)")
@@ -16,14 +18,20 @@ public class ClassLoaderAspect {
         JDOClassLoader enhancerClassLoader = new JDOClassLoader(persistenceClassLoader);
 
         Object target = joinPoint.getTarget();
+        BaseMdsService baseMdsService = null;
 
         if (target instanceof BaseMdsService) {
-            BaseMdsService baseMdsService = (BaseMdsService) target;
+            baseMdsService = (BaseMdsService) target;
             baseMdsService.setEnhancerClassLoader(enhancerClassLoader);
             baseMdsService.setPersistenceClassLoader(persistenceClassLoader);
         }
 
         joinPoint.proceed();
+
+        if (null != baseMdsService) {
+            baseMdsService.setEnhancerClassLoader(null);
+            baseMdsService.setPersistenceClassLoader(null);
+        }
 
         Thread.currentThread().setContextClassLoader(webAppClassLoader);
     }
