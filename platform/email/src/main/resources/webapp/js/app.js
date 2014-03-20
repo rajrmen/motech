7fd4fc2a6134a81a9f46d1839af5de5aedf87b5f
Łukasz Lewczyński <llewczynski@soldevelo.com>
2014-03-20 08:23:48 +0100

    back to ng-view in email
    Change-Id: I7d78b7bcf2a9bb6099e50948c9c4f73978164f8f

--- platform/email/src/main/resources/webapp/js/app.js
+++ platform/email/src/main/resources/webapp/js/app.js
@@ -6,21 +6,16 @@
     var app = angular.module('email', ['motech-dashboard', 'ngCookies', 'ngRoute',
         'email.controllers', 'email.directives', 'email.services']);
 
-    app.config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('email_send', {
-            url: '/send',
-            templateUrl: '../email/resources/partials/sendEmail.html',
-            controller: 'SendEmailController'
-        }).state('email_logging', {
-            url: '/logging',
-            templateUrl: '../email/resources/partials/emailLogging.html',
-            controller: 'EmailLoggingController'
-        }).state('email_settings', {
-            url: '/settings',
-            templateUrl: '../email/resources/partials/settings.html',
-            controller: 'SettingsController'
-        });
-
-        $urlRouterProvider.otherwise('/send');
+    app.config(function ($routeProvider) {
+        $routeProvider.when('/email/send', {
+                templateUrl: '../email/resources/partials/sendEmail.html',
+                controller: 'SendEmailController'
+            }).when('/email/logging', {
+                templateUrl: '../email/resources/partials/emailLogging.html',
+                controller: 'EmailLoggingController'
+            }).when('/email/settings', {
+                templateUrl: '../email/resources/partials/settings.html',
+                controller: 'SettingsController'
+            });
     });
 }());
