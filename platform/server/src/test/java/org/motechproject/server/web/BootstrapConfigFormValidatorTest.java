ac8abeb6110fcfb9dd5bff270945df202fdfaebb
Paweł Gesek <pgesek@soldevelo.com>
2014-07-25 10:58:53 +0200

    Fixed tests
    Change-Id: I3c9d8a6dd75ff74054d084da86d1f57d884c41da

--- platform/server/src/test/java/org/motechproject/server/web/BootstrapConfigFormValidatorTest.java
+++ platform/server/src/test/java/org/motechproject/server/web/BootstrapConfigFormValidatorTest.java
@@ -2,14 +2,21 @@ package org.motechproject.server.web;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.motechproject.server.web.validator.ValidationUtils;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.validation.Errors;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
+import static org.motechproject.server.web.BootstrapConfigFormValidator.ERROR_REQUIRED;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ValidationUtils.class)
 public class BootstrapConfigFormValidatorTest {
 
     @Mock
@@ -24,21 +31,10 @@ public class BootstrapConfigFormValidatorTest {
 
     @Test
     public void shouldValidateOnlyForEmptyFields() {
-        when(errors.hasFieldErrors("sqlUrl")).thenReturn(true);
-
         bootstrapConfigFormValidator.validate(new BootstrapConfigForm(), errors);
 
         verifyStatic();
-    }
-
-    @Test
-    public void shouldValidateForDbUrlFormatIfNotEmpty() {
-        when(errors.hasFieldErrors("sqlUrl")).thenReturn(false);
-        when(errors.getFieldValue("sqlbUrl")).thenReturn("jdbc://localhost");
-
-        bootstrapConfigFormValidator.validate(new BootstrapConfigForm(), errors);
-
-        verifyStatic();
+        ValidationUtils.validateEmptyOrWhitespace(errors, ERROR_REQUIRED, "sqlUrl");
     }
 
     @Test
